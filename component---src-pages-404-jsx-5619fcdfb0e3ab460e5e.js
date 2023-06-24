0f58d061bf796b013c10de4f7367bd50309e576f
Kojiro Ozaki <koujirou0111@gmail.com>
2023-06-25 02:38:05 +0900

    Updates

---
+++ component---src-pages-404-jsx-5619fcdfb0e3ab460e5e.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkyuhi_kyoto_com=self.webpackChunkyuhi_kyoto_com||[]).push([[256],{8336:function(t,e,n){n.r(e),n.d(e,{Head:function(){return u}});var a=n(2784),o=n(8165);e.default=()=>(0,o.tZ)(a.Fragment,null);const u=t=>{let{data:{site:{siteMetadata:{siteUrl:e}}}}=t;return(0,o.tZ)(a.Fragment,null,(0,o.tZ)("meta",{name:"robots",content:"noindex"}),(0,o.tZ)("meta",{"http-equiv":"Refresh",content:"0; URL="+e}))}}}]);
+//# sourceMappingURL=component---src-pages-404-jsx-5619fcdfb0e3ab460e5e.js.map
